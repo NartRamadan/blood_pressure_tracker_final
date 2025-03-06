fee59d067d9012ad232192fab0af14529ee67d41
NartRamadan <kataz91@gmail.com>
2025-03-06 13:12:27 +0200

    נקודת קצה להוספת משתמש פונקציה להוספת משתשמש למסד הנתונים

---
+++ Middleware/User_Mid.js
@@ -0,0 +1,23 @@
+async function AddUser(req,res,next){
+    let name   = req.body.name;
+
+    const Query = `INSERT INTO Users (name) VALUES('${name}')`;
+    // console.log(Query);
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.success=true;
+        req.insertId=rows.insertId;
+    } catch (err) {
+        console.log(err);
+        req.success=false;
+        req.insertId=-1;
+    }
+
+    next();
+}
+
+module.exports = {
+    AddUser: AddUser
+}
--- Routers/Users_R.js
+++ Routers/Users_R.js
@@ -2,4 +2,12 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 
-const User_Mid=require("../middleware/User_Mid");
+const User_Mid=require("../middleware/User_Mid")
+
+router.post('/AddUser',[User_Mid.AddUser], (req, res) => {
+    if(req.success){
+        res.status(200).json({msg:"ok",Last_Id:req.insertId});
+    } else {
+        return res.status(500).json({message: err});
+    }
+});
